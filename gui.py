7f918ae1585f2f9297bd81920637eee5294c378b
Bhat, Manu <manu.bhat@dell.com>
2021-02-01 09:34:08 +0530

    fixes enhncments

--- gui.py
+++ gui.py
@@ -1,32 +1,35 @@
 import sys
 import nsefetch
+from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QTableWidgetItem
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QVBoxLayout
 from PyQt5.QtWidgets import QPushButton
-from PyQt5.QtWidgets import QScrollArea
-from PyQt5.QtWidgets import QHBoxLayout
-
+from PyQt5.QtWidgets import QTableWidget
 nifty_eqts = ["MARUTI", "TECHM", "WIPRO", "UPL", "INFY", "EICHERMOT", "ULTRACEMCO", "POWERGRID", "HEROMOTOCO",
               "NTPC", "HCLTECH", "SHREECEM", "M&M", "SUNPHARMA", "TCS", "LT", "BAJAJ-AUTO", "ONGC", "COALINDIA",
               "SBILIFE", "BPCL", "BAJAJFINSV", "ASIANPAINT", "IOC", "HDFCLIFE", "DRREDDY", "DIVISLAB", "RELIANCE",
               "HDFCBANK", "CIPLA", "HINDUNILVR", "NESTLEIND", "KOTAKBANK", "GRASIM", "BRITANNIA", "TATAMOTORS",
               "ADANIPORTS", "TITAN", "AXISBANK", "ICICIBANK", "BAJFINANCE", "HDFC", "ITC", "SBIN", "JSWSTEEL",
               "GAIL", "BHARTIARTL", "TATASTEEL", "INDUSINDBK", "HINDALCO"]
+nifty_eqts.sort()
 
 page_size = 5
+
+main_layout = None
 
 
 def get_stock():
     if (page_size * current_page) == len(nifty_eqts):
         return []
     if current_page == 0:
-        ret_eqt = nifty_eqts[current_page:page_size - 1]
+        ret_eqt = nifty_eqts[current_page:page_size]
         return ret_eqt
     else:
         start = page_size * current_page
-        end = (page_size * (current_page + 1)) -1
+        end = (page_size * (current_page + 1))
         return nifty_eqts[start:end]
 
 
@@ -37,19 +40,20 @@ def open_window():
     window.setGeometry(800, 800, 280, 80)
     window.move(100, 100)
     window.resize(500, 500)
-    main_layout = QHBoxLayout()
-    scrollArea = QScrollArea()
-    scrollArea.setWidgetResizable(True)
-    helloMsg = QLabel("<h1>Buy Sell Signals</h1>", parent=window)
-    scrollContents = QWidget()
-    layout = QGridLayout(scrollContents)
-    layout.addWidget(helloMsg, 0, 1)
-    next_btn = QPushButton('Next');
-    next_btn.clicked.connect(lambda: get_next(layout))
-    layout.addWidget(next_btn, 1, 1)
-    scrollArea.setWidget(scrollContents)
-    main_layout.addWidget(scrollArea)
-    get_next(layout)
+    global main_layout
+    main_layout = QVBoxLayout()
+    hello_msg = QLabel("<h1>Buy Sell Signals</h1>", parent=window)
+    main_layout.addWidget(hello_msg, 0)
+    tableView = QTableWidget()
+    tableView.setEnabled(True)
+    tableView.setAlternatingRowColors(True)
+    tableView.setRowCount(5)
+    tableView.setColumnCount(2)
+    next_btn = QPushButton('Next')
+    next_btn.clicked.connect(lambda: get_next(tableView))
+    main_layout.addWidget(next_btn, 0)
+    get_next(tableView)
+    main_layout.addWidget(tableView)
     window.setLayout(main_layout)
     window.show()
 
@@ -61,15 +65,37 @@ current_page = 0
 
 
 def get_next(layout):
-    counter = layout.rowCount()
-    counter += 1
 
-    for eq in nsefetch.get_data_for(get_stock()):
-        layout.addWidget(QLabel(eq.get("name")), counter, 1)
-        layout.addWidget(QLabel(eq.get("signal")), counter, 2)
+    stocks = get_stock()
+    if len(stocks) > 0:
+        add_stock_to_table(stocks, layout)
+        global current_page
+        current_page += 1
+    else:
+        current_page = 0
+        stocks = get_stock()
+        add_stock_to_table(stocks, layout)
+        current_page += 1
+
+
+def add_stock_to_table(stocks, layout):
+    counter = 0
+    for eq in nsefetch.get_data_for(stocks):
+        signal = eq.get("signal")
+        layout.setItem(counter, 0, get_colored_item(signal, eq.get("name")))
+        layout.setItem(counter, 1, get_colored_item(signal, eq.get("signal")))
         counter += 1
-    global current_page
-    current_page += 1
+
+
+def get_colored_item(signal, wid_label):
+    color = QtGui.QColor(251, 251, 251)
+    if signal == "Buy":
+        color = QtGui.QColor(0, 204, 0)
+    elif signal == "Sell":
+        color = QtGui.QColor(255, 0, 0)
+    eq_t_w = QTableWidgetItem(wid_label)
+    eq_t_w.setBackground(color)
+    return eq_t_w
 
 
 if __name__ == "__main__":
--- nsefetch.py
+++ nsefetch.py
@@ -5,44 +5,59 @@ from datetime import timedelta
 
 def get_data_for(nifty_eqts):
     all_signals = []
+    start, end = get_start_end()
+    stock_data = nsepy.get_history(nifty_eqts[0], start, end)
+    if stock_data is not None and stock_data.iloc is not None:
+        try:
+            stock_data.iloc[1]
+        except IndexError:
+            start, end = get_start_end(-1)
     for eqt in nifty_eqts:
-        all_signals.append(print_stock_signal(eqt))
+        all_signals.append(print_stock_signal(eqt, start, end))
     return all_signals
 
 
-def print_stock_signal(eqt):
-    strtDt = datetime.date.today() - timedelta(days=2)
-    endDt = datetime.date.today() - timedelta(days=1)
-
-    if endDt.weekday() == 6:
-        strtDt = strtDt - timedelta(days=1)
-        endDt = endDt - timedelta(days=1)
-    elif endDt.weekday() == 0:
-        strtDt = strtDt - timedelta(days=2)
-        endDt = endDt - timedelta(days=2)
-    return get_signals(eqt, strtDt, endDt)
-
-
-def get_signals(eqt, strtDt, endDt):
-    stock_data = nsepy.get_history(eqt, strtDt, endDt)
+def print_stock_signal(eqt, start, end):
+    stock_data = nsepy.get_history(eqt, start, end)
     # print(stock_data)
-    symb = stock_data.iloc[0]["Symbol"]
-    close = stock_data.iloc[0]["Close"]
-    vwap = stock_data.iloc[0]["VWAP"]
-    volume = stock_data.iloc[0]["Volume"]
-    prevolume = stock_data.iloc[1]["Volume"]
-    sign = None
-    if ((volume - prevolume) / prevolume) > 0.1:
-        if vwap < close:
-            sign = "Buy"
-            print(symb + "--->" + sign)
+
+    return get_signals(stock_data)
+
+
+def get_start_end(ind=0):
+    begin_date = datetime.date.today()
+    if ind < 0:
+        begin_date = begin_date - timedelta(days=ind)
+    strt_dt = begin_date - timedelta(days=2)
+    end_dt = begin_date - timedelta(days=1)
+
+    if end_dt.weekday() == 6:
+        strt_dt = strt_dt - timedelta(days=2)
+        end_dt = end_dt - timedelta(days=2)
+    elif end_dt.weekday() == 5:
+        strt_dt = strt_dt - timedelta(days=1)
+        end_dt = end_dt - timedelta(days=1)
+    elif end_dt.weekday() == 0:
+        strt_dt = strt_dt - timedelta(days=3)
+    return strt_dt, end_dt
+
+
+def get_signals(stock_data):
+        symb = stock_data.iloc[0]["Symbol"]
+        close = stock_data.iloc[0]["Close"]
+        vwap = stock_data.iloc[0]["VWAP"]
+        volume = stock_data.iloc[0]["Volume"]
+        prevolume = stock_data.iloc[1]["Volume"]
+        sign = None
+        if ((volume - prevolume) / prevolume) > 0.1:
+            if vwap < close:
+                sign = "Buy"
+            else:
+                sign = "Sell"
         else:
-            sign = "Sell"
-            print(symb + "--->" + sign)
-    else:
-        sign = "WAIT"
-        print(symb + "--->" + "WAIT")
-    return {'name': symb, 'signal': sign}
+            sign = "WAIT"
+        print(symb + "--->" + sign)
+        return {'name': symb, 'signal': sign}
 
 
 
